04d92bad61cb683f32d93a98113100c859b5d104
shalini-2004 <shalini160204@gmail.com>
2024-04-29 19:24:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const endDate= ". . . . 2 May 2024 04:00 PM . . . ."
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+
+// const clock=()=>{}
+
+function clock(){
+    const end = new Date(endDate);
+    const now = new Date()
+    const gap = (end-now) / 1000;
+    if (gap < 0) return;
+
+    // here 3600 sec in 1 min  60 min in 1 hour and 24 hour in a day.........
+    inputs[0].value = Math.floor(gap / 3600 / 24)
+    inputs[1].value = Math.floor((gap / 3600) % 24)
+    inputs[2].value = Math.floor((gap / 60) % 60)
+    inputs[3].value = Math.floor(gap % 60)
+}
+clock()
+setInterval(
+    () => {
+        clock()
+    },1000
+);
+
+
+
+
